fc451018e3e36f4363ae0eaab6a9b0b0759db533
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-27 13:57:57 -0500

    added update table/ did not add drop and recreate yet

---
+++ update_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+-- SELECT 'Albums' As 'All';
+-- SELECT * from albums;
+-- UPDATE albums SET sales = sales * 10 WHERE sales;
+-- SELECT * from albums;
+
+-- SELECT 'Albums after 1980' AS 'All';
+-- SELECT * from albums WHERE release_date < 1980;
+-- UPDATE albums SET release_date = release_date - 100 WHERE release_date;
+-- SELECT * from albums WHERE release_date < 1980;
+
+-- SELECT 'Albums by Michael Jackson' AS 'All';
+-- Select * from albums WHERE artist = 'Michael Jackson';
+-- UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+-- Select * from albums WHERE artist = 'Peter Jackson';
