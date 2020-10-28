0efa7b94622920e613e077eb1994b8ea8fb08050
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-28 16:24:46 -0500

    finished basic statements exercise and began on clauses exercises

---
+++ delete_exercises.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+-- SELECT * FROM albums WHERE release_date > 1991;
+--
+-- select * from albums where genre = 'disco';
+--
+-- select * from albums where artist = 'Whitney Houston / Various artists';
+--
+-- delete from albums where release_date > 1991;
+--
+-- delete from albums where genre = 'disco';
+--
+-- delete from albums where artist = 'Whitney Houston / Various artists';
+
+SELECT * FROM albums;
