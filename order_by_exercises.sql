0aeff338bf075ce393f019eda1c0bbb92f474bf9
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-29 08:59:56 -0500

    ordered by last name then first name

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 
- SELECT * FROM employees where first_name IN ('Irena','Vidya','Maya') ORDER BY first_name, last_name;
+ SELECT * FROM employees where first_name IN ('Irena','Vidya','Maya') ORDER BY  last_name, first_name;
 
 #  SELECT * FROM employees WHERE last_name LIKE 'E%';
 #
