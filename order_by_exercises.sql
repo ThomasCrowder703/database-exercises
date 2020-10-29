3a398bb7c80fc43aefb4eb6adfa8692672a96d52
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-29 09:17:14 -0500

    completed order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,8 +1,8 @@
 USE employees;
 
- SELECT * FROM employees where first_name IN ('Irena','Vidya','Maya') ORDER BY  last_name, first_name;
+ SELECT * FROM employees where first_name IN ('Irena','Vidya','Maya') ORDER BY  last_name, first_name DESC;
 
-#  SELECT * FROM employees WHERE last_name LIKE 'E%';
+ SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC;
 #
 #  SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 #
@@ -16,6 +16,6 @@ USE employees;
 #
 #  SELECT * FROM employees where last_name LIKE '%E' AND last_name LIKE 'E%';
 #
-#  SELECT * FROM employees WHERE hire_date between '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
+ SELECT * FROM employees WHERE hire_date between '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date DESC;
 #
 #  SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
