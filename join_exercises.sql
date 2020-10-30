1ac9bfd295f69a1e817c7909c524a9b5013c337e
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-30 11:04:12 -0500

    walkthrough on exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 
-SELECT e.first_name as name, d.dept_name #How does it know to find those specific people
+SELECT e.first_name as name, d.dept_name #How does it know to find those specific people A:Its grabbing all the active managers
 FROM employees as e
 JOIN dept_manager as dm
 ON e.emp_no = dm.emp_no
@@ -16,5 +16,27 @@ ON e.emp_no = dm.emp_no
 JOIN departments as d
 ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'f'
-ORDER BY d.dept_name
+ORDER BY d.dept_name;
+
+SELECT t.title AS Title, COUNT(t.emp_no) AS Count
+FROM titles AS t
+JOIN dept_emp AS de ON t.emp_no = de.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE de.dept_no = 'd009'
+  AND t.to_date = '9999-01-01'
+  AND de.to_date = '9999-01-01'
+GROUP BY t.title;
+
+SELECT d.dept_name AS 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
+       s.salary AS Salary
+FROM employees AS e
+JOIN dept_manager AS dm ON e.emp_no = dm.emp_no #look at diagrams to see relationships
+JOIN departments AS d ON dm.dept_no = d.dept_no
+JOIN salaries AS s ON e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
+
+
+
 
---
+++ subqueries_exercises.sql
@@ -0,0 +1,28 @@
+USE employees;
+
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name
+FROM employees
+    WHERE hire_date =(SELECT hire_date
+                      FROM employees
+                      WHERE emp_no = 101010);
+
+SELECT title, emp_no
+FROM titles
+WHERE emp_no IN ( SELECT emp_no
+                 FROM employees
+                 WHERE first_name = 'Aamod');
+
+
+#TO find how many unique titles
+# SELECT COUNT(title), title
+# FROM titles
+# WHERE emp_no IN (SELECT emp_no
+#                  FROM employees
+#                  WHERE first_name = 'Aamod')
+#
+# GROUP BY title;
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, gender
+FROM employees
+WHERE emp_no IN (SELECT emp_no
+FROM dept_manager
+WHERE to_date = '9999-01-01' AND gender ='f');
