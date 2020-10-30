db225b6c36dac84c44799a3dd0d450b4765539bd
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-30 13:41:08 -0500

    completed final exercise for group by/ still need to use order by on previous exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -5,5 +5,6 @@ SELECT DISTINCT title FROM titles;
 
 SELECT first_name , last_name FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' GROUP BY first_name, last_name;
 
-SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ;
+SELECT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ; #How do I use order by in this case
 
+SELECT gender, COUNT(*) FROM employees where first_name IN ('Irena','Vidya','Maya') GROUP BY gender;
