90f309b3abff69704dcddd5d3f9723c783352346
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-30 13:53:19 -0500

    finished functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -16,6 +16,6 @@ USE employees;
 #
  SELECT CONCAT(first_name, ' ', last_name) FROM employees where last_name LIKE '%E' AND last_name LIKE 'E%';
 #
-#  SELECT * FROM employees WHERE hire_date between '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25' ORDER BY birth_date, hire_date DESC;
+ SELECT DISTINCT DATEDIFF(NOW() , '1990-01-01') FROM employees WHERE hire_date between '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
 #
 #  SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
