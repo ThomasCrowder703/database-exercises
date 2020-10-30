c4f0830146a9b1291edad10f39d58d6b597050a1
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-30 13:35:26 -0500

    grouped by unique last names

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -3,5 +3,7 @@ USE employees;
 SELECT DISTINCT title FROM titles;
 
 
-SELECT first_name , last_name FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' group by first_name, last_name;
+SELECT first_name , last_name FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ;
 
